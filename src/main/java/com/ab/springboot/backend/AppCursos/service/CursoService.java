ea665828e335d241e4951d0ac6fab2306e3bbea4
NoeFlores <floresaserico.n@gmail.com>
2021-10-20 19:26:05 -0400

    Listar cursos registrados

---
+++ src/main/java/com/ab/springboot/backend/AppCursos/service/CursoService.java
@@ -0,0 +1,19 @@
+package com.ab.springboot.backend.AppCursos.service;
+
+import java.util.List;
+
+import com.ab.springboot.backend.AppCursos.model.Curso;
+
+
+
+public interface CursoService {
+	
+	public Curso save(Curso curso);
+	
+	public void delete(Integer id);
+	
+	public Curso findById(Integer id);
+	
+	public List<Curso> findAll();
+
+}
