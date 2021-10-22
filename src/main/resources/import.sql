8b4524aba27cbaf417878c80f9c2e7da753cc2c8
NoeFlores <floresaserico.n@gmail.com>
2021-10-22 17:16:03 -0400

    archivo sql borrado

--- src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-insert into cursos(nombre_intructor, nombre) values('Juanita Pa', 'Inteligencia Artifical');
-insert into cursos(nombre_intructor, nombre) values('Juanito Perez', 'Programación Java');
-insert into cursos(nombre_intructor, nombre) values('Jaimito Lu', 'Programación Basica');
