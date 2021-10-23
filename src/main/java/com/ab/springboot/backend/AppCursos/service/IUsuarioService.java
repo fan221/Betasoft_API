fb9659d5b424306b50673540c198663dd465a9e5
NoeFlores <floresaserico.n@gmail.com>
2021-10-23 09:04:56 -0400

    modificacion del método service

---
+++ src/main/java/com/ab/springboot/backend/AppCursos/service/IUsuarioService.java
@@ -0,0 +1,12 @@
+package com.ab.springboot.backend.AppCursos.service;
+
+
+
+import com.ab.springboot.backend.AppCursos.model.Usuario;
+
+
+public interface IUsuarioService {
+	public Usuario finByUsername(String usuario);
+
+}
+
