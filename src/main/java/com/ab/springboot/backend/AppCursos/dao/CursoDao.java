263a4bbdce0e01e436d6222ae3afa1d6ce9a4703
NoeFlores <floresaserico.n@gmail.com>
2021-10-24 03:08:51 -0400

    acceso a DB

---
+++ src/main/java/com/ab/springboot/backend/AppCursos/dao/CursoDao.java
@@ -0,0 +1,10 @@
+package com.ab.springboot.backend.AppCursos.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ab.springboot.backend.AppCursos.model.Curso;
+
+public interface CursoDao extends CrudRepository<Curso, Integer>{
+	
+
+}
