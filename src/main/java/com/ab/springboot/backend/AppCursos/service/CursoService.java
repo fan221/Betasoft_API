e25b9518896bf89d837e715ceb34d49b3fe0d0d1
NoeFlores <floresaserico.n@gmail.com>
2021-10-24 04:07:02 -0400

    se actualizo los metodos

--- src/main/java/com/ab/springboot/backend/AppCursos/service/CursoService.java
+++ src/main/java/com/ab/springboot/backend/AppCursos/service/CursoService.java
@@ -8,9 +8,9 @@ public interface CursoService {
 	
 	public Curso save(Curso curso);
 	
-	public void delete(Integer id);
+	public void delete(Integer id_curso);
 	
-	public Curso findById(Integer id);
+	public Curso findById(Integer id_curso);
 	
 	public List<Curso> findAll();
 
