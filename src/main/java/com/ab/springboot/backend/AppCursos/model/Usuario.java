4b9b3ebfb0287c8f95fa45c50c5f2f98aee56914
NoeFlores <floresaserico.n@gmail.com>
2021-10-23 20:35:01 -0400

    implementación clase Usuario para login

--- src/main/java/com/ab/springboot/backend/AppCursos/model/Usuario.java
+++ src/main/java/com/ab/springboot/backend/AppCursos/model/Usuario.java
@@ -12,23 +12,24 @@ import javax.persistence.Table;
 @Table(name = "Usuario")
 public class Usuario implements Serializable {
 	
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nombre;
 	private String apellido;
 	private String email;
-	private String usuario;
+	private String username;
 	private String contraseña;
 	
 	
-	public Usuario(Long id, String nombre, String apellido, String email, String username, String password) {
+	public Usuario(Long id, String nombre, String apellido, String email, String username, String contraseña) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.email = email;
-		this.usuario = usuario;
+		this.username = username;
 		this.contraseña = contraseña;
 	}
 	
@@ -39,40 +40,50 @@ public class Usuario implements Serializable {
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getUsuario() {
-		return usuario;
+
+	public String getUsername() {
+		return username;
 	}
-	public void setUsername(String usuario) {
-		this.usuario = usuario;
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
+
 	public String getContraseña() {
 		return contraseña;
 	}
+
 	public void setContraseña(String contraseña) {
 		this.contraseña = contraseña;
 	}
 	private static final long serialVersionUID = 1L;
-	
 
 }
