9513b10df64fc9fd6cb38b32e63db1e256be340c
NoeFlores <floresaserico.n@gmail.com>
2021-10-23 20:36:47 -0400

    configuración del servidor

--- src/main/java/com/ab/springboot/backend/AppCursos/security/SecurityConfig.java
+++ src/main/java/com/ab/springboot/backend/AppCursos/security/SecurityConfig.java
@@ -1,27 +1,43 @@
 package com.ab.springboot.backend.AppCursos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConf extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		super.configure(auth);
+		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+		
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
+		http.authorizeRequests().antMatchers("/","/auth/**","/public/**","/css/**","/js/**").permitAll().anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/auth/login").defaultSuccessUrl("/private/index",true).failureUrl("/auth/login?error=true")
+	.and()
+		.logout().logoutUrl("/logout").logoutSuccessUrl("/public/index");
 	}
+
 	
-	
+
 
 }
