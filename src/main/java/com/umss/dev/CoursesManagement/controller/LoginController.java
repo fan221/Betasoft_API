79174fef2d4cdc7e6e8d3ba2f009b55d8452a7b4
NoeFlores <floresaserico.n@gmail.com>
2021-11-03 03:51:28 -0400

    se borro una clase de prueba

--- src/main/java/com/umss/dev/CoursesManagement/controller/LoginController.java
+++ src/main/java/com/umss/dev/CoursesManagement/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.umss.dev.CoursesManagement.controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.umss.dev.CoursesManagement.model.Usuario;
 
