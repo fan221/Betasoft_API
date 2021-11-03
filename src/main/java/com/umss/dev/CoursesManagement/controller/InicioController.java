6d96842c4022b4658d5a9c6f476d868145e46d38
NoeFlores <floresaserico.n@gmail.com>
2021-11-03 03:41:39 -0400

    controlador de ini

--- src/main/java/com/umss/dev/CoursesManagement/controller/InicioController.java
+++ src/main/java/com/umss/dev/CoursesManagement/controller/InicioController.java
@@ -1,9 +1,10 @@
 package com.umss.dev.CoursesManagement.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class InicioController {
 	
 	@GetMapping("/")
