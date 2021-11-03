e9433e90d52db5657f550e4f0d9ec1d1ad1b341f
NoeFlores <floresaserico.n@gmail.com>
2021-11-03 03:56:01 -0400

    se creo la clase para el uso de neuvo curso

---
+++ src/main/java/com/umss/dev/CoursesManagement/payload/request/CrearRequest.java
@@ -0,0 +1,44 @@
+package com.umss.dev.CoursesManagement.payload.request;
+
+import com.umss.dev.CoursesManagement.model.Instructor;
+
+public class CrearRequest {
+
+	private String nombre;
+	private String descripcion;
+	private String ubicacion;
+	private Long instructor;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public String getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(String ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+
+	public Long  getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Long   instructor) {
+		this.instructor = instructor;
+	}
+}
