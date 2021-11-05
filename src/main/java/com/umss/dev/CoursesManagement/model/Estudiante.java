c070de5383cae0cf0e20641ad7b29f356807ab36
NoeFlores <floresaserico.n@gmail.com>
2021-11-05 15:11:04 -0400

    clase estudiante modificado

--- src/main/java/com/umss/dev/CoursesManagement/model/Estudiante.java
+++ src/main/java/com/umss/dev/CoursesManagement/model/Estudiante.java
@@ -19,24 +19,25 @@ public class Estudiante {
 	
 	
 
-	@OneToMany(cascade = { CascadeType.ALL })
-	@JoinColumn(name = "id_instructor")
-	@JsonView({Views.instructorViews.class, Views.usuarioViews.class})
-	private Set<Curso> cursos;
+	
+	//@OneToMany(cascade = { CascadeType.ALL })
+	//@JoinColumn(name = "id_instructor")
+	//@JsonView({Views.instructorViews.class, Views.usuarioViews.class})
+	//private Set<Curso> cursos;
 
 	@OneToMany(cascade = { CascadeType.ALL })
-	@JoinColumn(name = "id_instructor")
+	@JoinColumn(name = "id_estudiante")
 	private Set<Usuario> usuarios;
 
 	public Estudiante() {
 		super();
 	}
 
-	public Estudiante(String nombre, String apellido_paterno, String apellido_materno, int ci, int celular,
-			String email, Set<Curso> cursos_instructor) {
+	public Estudiante(String nombre_completo)
+			 {
 		super();
-		this.nombre_completo = nombre;
-		this.cursos = cursos_instructor;
+		this.nombre_completo = nombre_completo;
+		//this.cursos = cursos_instructor;
 	}
 
 	public long getId() {
@@ -56,13 +57,13 @@ public class Estudiante {
 	}
 
 
-	public Set<Curso> getcursos() {
+	/**public Set<Curso> getcursos() {
 		return this.cursos;
 	}
 
 	public void setCursos(Set<Curso> cursos) {
 		this.cursos = cursos;
-	}
+	}*/
 
 	
 
