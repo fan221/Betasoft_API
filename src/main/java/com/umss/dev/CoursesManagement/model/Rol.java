29a7b69d5133521af36e327a148d93d015ecd6c6
NoeFlores <floresaserico.n@gmail.com>
2021-11-05 15:56:11 -0400

    clase rol modificado

--- src/main/java/com/umss/dev/CoursesManagement/model/Rol.java
+++ src/main/java/com/umss/dev/CoursesManagement/model/Rol.java
@@ -1,5 +1,27 @@
 package com.umss.dev.CoursesManagement.model;
 
-public class Rol {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+
+
+@Entity
+@Table(name = "roles")
+public class Rol {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+
+
+	public Rol() {
+
+	}
 }
+
