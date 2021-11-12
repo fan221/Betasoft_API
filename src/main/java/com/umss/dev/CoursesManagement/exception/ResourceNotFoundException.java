68f9f2eb1003360ce2a13640bf30ba32b8ac1b4d
NoeFlores <floresaserico.n@gmail.com>
2021-11-12 16:41:04 -0400

    constructor corregido

--- src/main/java/com/umss/dev/CoursesManagement/exception/ResourceNotFoundException.java
+++ src/main/java/com/umss/dev/CoursesManagement/exception/ResourceNotFoundException.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-    private String resourceName;
-    private String fieldName;
-    private Object fieldValue;
+    private String nombreRecurso;
+    private String nombreArchivo;
+    private Object valor;
 
 
-public ResourceNotFoundException( String resourceName, String fieldName, Object fieldValue) {
-    super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
-    this.resourceName = resourceName;
-    this.fieldName = fieldName;
-    this.fieldValue = fieldValue;
+public ResourceNotFoundException( String nombreRecurso, String nombreArchivo, Object valor) {
+    super(String.format("%s not found with %s : '%s'", nombreRecurso, nombreArchivo, valor));
+    this.nombreRecurso = nombreRecurso;
+    this.nombreArchivo = nombreArchivo;
+    this.valor = valor;
 }
 }
