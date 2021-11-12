fa0fdffa53f1687f2a5641482af236df9d958f32
NoeFlores <floresaserico.n@gmail.com>
2021-11-12 16:33:18 -0400

    atributos creados

--- src/main/java/com/umss/dev/CoursesManagement/exception/ResourceNotFoundException.java
+++ src/main/java/com/umss/dev/CoursesManagement/exception/ResourceNotFoundException.java
@@ -1,5 +1,8 @@
 package com.umss.dev.CoursesManagement.exception;
 
-public class ResourceNotFoundException {
-
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
 }
