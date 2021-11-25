a9b55d5ef2603392449b307c699d61a1f697b467
NoeFlores <floresaserico.n@gmail.com>
2021-11-25 16:10:20 -0400

    crear nuevo instructor

--- src/main/java/com/umss/dev/CoursesManagement/controller/InstructorController.java
+++ src/main/java/com/umss/dev/CoursesManagement/controller/InstructorController.java
@@ -3,14 +3,19 @@ package com.umss.dev.CoursesManagement.controller;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import com.umss.dev.CoursesManagement.model.Curso;
 import com.umss.dev.CoursesManagement.model.Instructor;
 import com.umss.dev.CoursesManagement.model.Views;
 import com.umss.dev.CoursesManagement.service.InstructorService;
@@ -33,6 +38,11 @@ public class InstructorController {
 	@JsonView(Views.instructorViews.class)
 	public Optional<Instructor> obtenerInstructor(@PathVariable Long id) {
 		return instructorService.findById(id);
+	}
+	
+	@PostMapping("/NewInstructor")
+	public Instructor createCurso(@Valid @RequestBody Instructor instructor) {
+		return instructorRepository.save(instructor);
 	}
 
 }
