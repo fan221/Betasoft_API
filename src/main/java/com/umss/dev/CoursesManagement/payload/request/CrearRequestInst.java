cbe22c0bcb1f119b7da6d38718154291c3604907
NoeFlores <floresaserico.n@gmail.com>
2021-11-26 14:09:12 -0400

    campo apellido_materno agregado

--- src/main/java/com/umss/dev/CoursesManagement/payload/request/CrearRequestInst.java
+++ src/main/java/com/umss/dev/CoursesManagement/payload/request/CrearRequestInst.java
@@ -3,5 +3,6 @@ package com.umss.dev.CoursesManagement.payload.request;
 public class CrearRequestInst {
 	private String nombre;
 	private String apellido_paterno;
+	private String apellido_materno;
 
 }
