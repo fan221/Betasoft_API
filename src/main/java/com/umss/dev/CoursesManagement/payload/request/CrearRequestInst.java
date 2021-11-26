9376192e5bf13078c8bf9d6bd4d6052c03bbe06e
NoeFlores <floresaserico.n@gmail.com>
2021-11-26 15:00:30 -0400

    métodos getters y setters agrgados

--- src/main/java/com/umss/dev/CoursesManagement/payload/request/CrearRequestInst.java
+++ src/main/java/com/umss/dev/CoursesManagement/payload/request/CrearRequestInst.java
@@ -18,4 +18,60 @@ public class CrearRequestInst {
 	private String area_especializacion;
 	private String nivel_estudio;
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido_paterno() {
+		return apellido_paterno;
+	}
+
+	public void setApellido_paterno(String apellido_paterno) {
+		this.apellido_paterno = apellido_paterno;
+	}
+
+	public String getApellido_materno() {
+		return apellido_materno;
+	}
+
+	public void setApellido_materno(String apellido_materno) {
+		this.apellido_materno = apellido_materno;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+
+	public void setFecha_nacimiento(Date fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+
+	public String getArea_especializacion() {
+		return area_especializacion;
+	}
+
+	public void setArea_especializacion(String area_especializacion) {
+		this.area_especializacion = area_especializacion;
+	}
+
+	public String getNivel_estudio() {
+		return nivel_estudio;
+	}
+
+	public void setNivel_estudio(String nivel_estudio) {
+		this.nivel_estudio = nivel_estudio;
+	}
+
 }
