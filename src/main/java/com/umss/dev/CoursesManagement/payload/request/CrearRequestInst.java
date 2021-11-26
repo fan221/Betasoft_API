b14057944b5d55da493498e2aa0d37759775d642
NoeFlores <floresaserico.n@gmail.com>
2021-11-26 14:19:50 -0400

    campor fecha_nacimiento agregado

--- src/main/java/com/umss/dev/CoursesManagement/payload/request/CrearRequestInst.java
+++ src/main/java/com/umss/dev/CoursesManagement/payload/request/CrearRequestInst.java
@@ -1,9 +1,19 @@
 package com.umss.dev.CoursesManagement.payload.request;
 
+import java.util.Date;
+
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 public class CrearRequestInst {
 	private String nombre;
 	private String apellido_paterno;
 	private String apellido_materno;
 	private String email;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date fecha_nacimiento;
 
 }
