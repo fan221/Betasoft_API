364c63a5396b9723d4167173f026e249d3d4a5fb
NoeFlores <floresaserico.n@gmail.com>
2021-11-27 18:03:37 -0400

    campos creados para nuevo user

--- src/main/java/com/umss/dev/CoursesManagement/payload/request/CrearUserRequest.java
+++ src/main/java/com/umss/dev/CoursesManagement/payload/request/CrearUserRequest.java
@@ -1,5 +1,44 @@
 package com.umss.dev.CoursesManagement.payload.request;
 
+import com.umss.dev.CoursesManagement.model.Estudiante;
+import com.umss.dev.CoursesManagement.model.Instructor;
+
 public class CrearUserRequest {
+	private String username;
+	private String passwd;
+	private Instructor instructor;
+	private Estudiante estudiante;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
+
+	public Estudiante getEstudiante() {
+		return estudiante;
+	}
+
+	public void setEstudiante(Estudiante estudiante) {
+		this.estudiante = estudiante;
+	}
 
 }
