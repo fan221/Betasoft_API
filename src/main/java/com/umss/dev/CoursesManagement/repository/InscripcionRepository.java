01635e053929d822628d7406e1032f39ac94025b
NoeFlores <floresaserico.n@gmail.com>
2021-11-27 21:15:35 -0400

    @Query agregado

--- src/main/java/com/umss/dev/CoursesManagement/repository/InscripcionRepository.java
+++ src/main/java/com/umss/dev/CoursesManagement/repository/InscripcionRepository.java
@@ -3,6 +3,7 @@ package com.umss.dev.CoursesManagement.repository;
 //import java.util.List;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.umss.dev.CoursesManagement.model.Inscripcion;
@@ -11,6 +12,7 @@ import com.umss.dev.CoursesManagement.model.Inscripcion;
 @Repository
 public interface InscripcionRepository extends JpaRepository<Inscripcion, Long> {
 	 //Optional<Inscripcion> findOne(Long id_curso);
+	@Query(value = "SELECT count(id_inscripcion) FROM Inscripcion where id_curso = :id_curso")
 	public Long count(@Param("id_curso") long id_curso);
 	
 	
