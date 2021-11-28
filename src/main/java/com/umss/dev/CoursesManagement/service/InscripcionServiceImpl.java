801a5a24ff5952c751f1b5487c8c26f31e0a7a88
NoeFlores <floresaserico.n@gmail.com>
2021-11-27 21:34:51 -0400

    método implements terminado

--- src/main/java/com/umss/dev/CoursesManagement/service/InscripcionServiceImpl.java
+++ src/main/java/com/umss/dev/CoursesManagement/service/InscripcionServiceImpl.java
@@ -1,5 +1,25 @@
 package com.umss.dev.CoursesManagement.service;
 
-public class InscripcionServiceImpl {
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.umss.dev.CoursesManagement.repository.InscripcionRepository;
+
+@Transactional
+@Service("inscripcionCountService")
+public class InscripcionServiceImpl implements InscripcionCountService {
+	
+	@Autowired
+	private InscripcionRepository inscripcionRepository;
+	
+	@Override
+	public Long count(long id_curso) {
+		return inscripcionRepository.count(id_curso);
+		
+	}
+	
+	 
 
 }
