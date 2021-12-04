65350ecc6e8368b75c53f3e92c3e72461eeabc1e
NoeFlores <floresaserico.n@gmail.com>
2021-12-03 23:20:59 -0400

    buscando email en DB

--- src/main/java/com/umss/dev/CoursesManagement/repository/InstructorRepository.java
+++ src/main/java/com/umss/dev/CoursesManagement/repository/InstructorRepository.java
@@ -1,12 +1,16 @@
 package com.umss.dev.CoursesManagement.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.umss.dev.CoursesManagement.model.Instructor;
 
 @Repository
 
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
-	
+	Optional<Instructor> findByEmail(String email);
+	Boolean existsByEmail(String email);	
 }
