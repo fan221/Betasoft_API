0d9b56bdf10390a680b17f96156c43469c96cc0a
Noemi Flores Aserico <92472202+Floresn03@users.noreply.github.com>
2021-12-05 17:16:59 -0400

    Revert "Revert "Contador inscritos""

--- src/main/java/com/umss/dev/CoursesManagement/repository/InstructorRepository.java
+++ src/main/java/com/umss/dev/CoursesManagement/repository/InstructorRepository.java
@@ -1,9 +1,11 @@
 package com.umss.dev.CoursesManagement.repository;
 
-
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.umss.dev.CoursesManagement.model.Instructor;
@@ -11,6 +13,10 @@ import com.umss.dev.CoursesManagement.model.Instructor;
 @Repository
 
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
+
+	 @Query(nativeQuery = true,value = "call cursos_instructor")  
+	    List<Map<String, Object>> cursosInstructor();
+
 	Optional<Instructor> findByEmail(String email);
 	Boolean existsByEmail(String email);	
 }
