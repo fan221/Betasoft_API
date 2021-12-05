ae74e422ddaf94b4a51c7d30b08a1f7c5b2d92ef
Mariela-Fernandez297 <fernandezrealmariela@gmail.com>
2021-12-05 13:25:33 -0400

    cambiada la api instructor

--- src/main/java/com/umss/dev/CoursesManagement/repository/InstructorRepository.java
+++ src/main/java/com/umss/dev/CoursesManagement/repository/InstructorRepository.java
@@ -1,12 +1,17 @@
 package com.umss.dev.CoursesManagement.repository;
 
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.umss.dev.CoursesManagement.model.Instructor;
 
 @Repository
 
 public interface InstructorRepository extends JpaRepository<Instructor, Long> {
-	
+	 @Query(nativeQuery = true,value = "call cursos_instructor")  
+	    List<Map<String, Object>> cursosInstructor();
 }
